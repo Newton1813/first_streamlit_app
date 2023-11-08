5d28530c97a1ab9cfcadb31d3e1dc2af96b27f79
Newton <newtonmckvie@gmail.com>
2023-11-08 21:49:01 +0530

    Update streamlit_file.py

--- streamlit_file.py
+++ streamlit_file.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('BlueBerry and Oat Meal')
+streamlit.text('Spinach ,Letuce and cauliflower')
+
+streamlit.text('Hard Boiled Egg')
