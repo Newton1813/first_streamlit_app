4bd254e9292f87beb325e746fce20f0cd8391497
Newton <newtonmckvie@gmail.com>
2023-11-08 21:53:31 +0530

    Update streamlit_file.py

--- streamlit_file.py
+++ streamlit_file.py
@@ -1,7 +1,12 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
-streamlit.header('🥣 🥗 🐔 🥑🍞Breakfast Menu🥣 🥗 🐔 🥑🍞')
-streamlit.text('🥣 🥗 🐔 🥑🍞Omega 3 & Blueberry Oatmeal🥣 🥗 🐔 🥑🍞')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header(' 🐔 Breakfast Menu🐔 ')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal🥣')
+streamlit.text(' 🥗 🐔 Kale, Spinach & Rocket Smoothie 🥗 🐔 ')
+streamlit.text(' 🥗 🐔 Hard-Boiled Free-Range Egg 🥗 🐔 ')
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
