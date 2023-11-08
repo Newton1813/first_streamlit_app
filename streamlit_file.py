17c04a5c9c8f06c30608e1e486fcb201abb6534c
Newton <newtonmckvie@gmail.com>
2023-11-08 21:50:36 +0530

    Update streamlit_file.py

--- streamlit_file.py
+++ streamlit_file.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.header('🥣 🥗 🐔 🥑🍞Breakfast Menu🥣 🥗 🐔 🥑🍞')
+streamlit.text('🥣 🥗 🐔 🥑🍞Omega 3 & Blueberry Oatmeal🥣 🥗 🐔 🥑🍞')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
